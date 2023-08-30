7e50eb51cb159b4c186bfef37e60086c03d26262
mrsins02 <amin.d.d.y1357@gmail.com>
2023-08-30 06:11:29 +0330

    twitter and facebook auth removed

--- src/config/common.py
+++ src/config/common.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = (
     # Your apps
     'src.notifications',
     'src.users',
-    'src.social',
     'src.files',
     'src.common',
     'src.business',
@@ -236,17 +235,8 @@ AUTHENTICATION_BACKENDS = (
     'src.users.backends.EmailOrUsernameModelBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
-for key in ['GOOGLE_OAUTH2_KEY', 'GOOGLE_OAUTH2_SECRET', 'FACEBOOK_KEY', 'FACEBOOK_SECRET', 'TWITTER_KEY',
-            'TWITTER_SECRET']:
+for key in ['GOOGLE_OAUTH2_KEY', 'GOOGLE_OAUTH2_SECRET']:
     exec("SOCIAL_AUTH_{key} = os.environ.get('{key}', '')".format(key=key))
-
-# FB
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id, name, email'}
-SOCIAL_AUTH_FACEBOOK_API_VERSION = '5.0'
-
-# Twitter
-SOCIAL_AUTH_TWITTER_SCOPE = ['email']
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
 SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'email']
@@ -264,21 +254,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
-)
-
-SOCIAL_AUTH_TWITTER_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    # 'social_core.pipeline.social_auth.social_user',
-    'src.common.social_pipeline.user.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-    'src.common.social_pipeline.user.login_user',  # login correct user at the end
 )
 
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/complete/twitter/'
