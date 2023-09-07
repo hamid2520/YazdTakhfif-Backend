3225c40f97423a59cf2fa65ff58f00ce4bf64d0e
mrsins02 <amin.d.d.y1357@gmail.com>
2023-09-07 23:56:50 +0330

    fixed basket signal

--- src/basket/signals.py
+++ src/basket/signals.py
@@ -8,14 +8,15 @@ from .models import Basket
 @receiver(m2m_changed, sender=Basket.product.through)
 def basket_products_price_updater(sender, **kwargs):
     basket: Basket = kwargs["instance"]
-    basket.count = basket.product.all().count()
     if not basket.is_paid:
-        basket.total_price = basket.product.all().aggregate(Sum("total_price"))["total_price__sum"] or 0
+        basket.count = basket.product.all().aggregate(Sum("count"))["count__sum"]
+        basket.count = basket.count if basket.count else 0
+        basket.total_price = basket.product.all().aggregate(Sum("total_price"))["total_price__sum"]
+        basket.total_price = basket.total_price if basket.total_price else 0
         basket.total_price_with_offer = basket.product.all().aggregate(Sum("total_price_with_offer"))[
-                                            "total_price_with_offer__sum"] or 0
+            "total_price_with_offer__sum"]
+        basket.total_price_with_offer = basket.total_price_with_offer if basket.total_price_with_offer else 0
+        basket.total_offer_percent = (
+                100 - (basket.total_price_with_offer * 100 / basket.total_price)) if basket.total_price else 0
 
-        if basket.total_price:
-            basket.total_offer_percent = (100 - (basket.total_price_with_offer * 100 / basket.total_price))
-        else:
-            basket.total_offer_percent = 0
     basket.save()
