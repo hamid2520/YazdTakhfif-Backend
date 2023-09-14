50ffce700c720f57d1b13eb159dd42f7d050f8af
mrsins02 <amin.d.d.y1357@gmail.com>
2023-09-14 20:00:32 +0330

    created customer_views for coupon,lines and category

---
+++ src/coupon/customer_views.py
@@ -0,0 +1,36 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
+from rest_framework.settings import api_settings
+from rest_framework.mixins import RetrieveModelMixin
+
+from .models import Category, Coupon, LineCoupon
+from .serializers import CategorySerializer, CouponSerializer, LineCouponSerializer
+
+
+class CategoryAPIView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = api_settings.DEFAULT_FILTER_BACKENDS + [SearchFilter, ]
+    search_fields = ['title', ]
+
+
+class CouponAPIView(RetrieveModelMixin, ListAPIView):
+    queryset = Coupon.objects.all()
+    serializer_class = CouponSerializer
+    lookup_field = "slug"
+    lookup_url_kwarg = "slug"
+    filter_backends = api_settings.DEFAULT_FILTER_BACKENDS + [SearchFilter, ]
+    search_fields = ['title', "linecoupon__title"]
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get("slug"):
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+
+class LineCouponAPIView(ListAPIView):
+    serializer_class = LineCouponSerializer
+
+    def get_queryset(self):
+        coupon = Coupon.objects.get(slug=self.kwargs.get("coupon_slug"))
+        return coupon.linecoupon_set.all()
