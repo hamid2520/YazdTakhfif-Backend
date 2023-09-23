aae1658fea1cdb4356382a61cd54aa80cc675c2d
mrsins02 <amin.d.d.y1357@gmail.com>
2023-09-23 14:24:27 +0330

    action for validate offer created

--- src/offer/serializers.py
+++ src/offer/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from .models import Offer
+from ..basket.models import Basket
 from ..business.models import Business
 
 
@@ -17,3 +18,20 @@ class OfferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Offer
         fields = ["offer_code", "percent", "start_date", "expire_date", "limited_businesses", "maximum_offer_price"]
+
+
+class OfferValidatorSerializer(serializers.Serializer):
+    offer_code = serializers.CharField(max_length=16)
+    basket_slug = serializers.SlugField()
+
+    def validate_offer_code(self, value):
+        offer = Offer.objects.filter(offer_code=value)
+        if offer.exists():
+            return value
+        raise ValidationError("Offer code is not valid!")
+
+    def validate_basket_slug(self, value):
+        basket = Basket.objects.filter(slug=value)
+        if basket.exists():
+            return value
+        raise ValidationError("Basket does not exists!")
