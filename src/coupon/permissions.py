0413f1e86a1525937d7bef72802a2fbd383a5b55
mrsins02 <amin.d.d.y1357@gmail.com>
2023-10-04 11:00:59 +0330

    bugs fixed

--- src/coupon/permissions.py
+++ src/coupon/permissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsSuperUserOrOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
-        return bool(user.is_superuser or obj.user_id == user.id)
+        return bool(user.is_superuser or obj.user_id == user.id or obj.coupon.business.admin_id == user.id)
