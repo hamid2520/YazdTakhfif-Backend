f75606c72f66493310d6a6b5f0dc76bc9f05e7f0
Alireza <areza4693@gmail.com>
2023-10-29 14:04:21 +0330

    api list slider

--- src/advertise/views.py
+++ src/advertise/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
 
 from .models import Advertise
 from .serializers import AdvertiseSerializer
@@ -7,5 +8,11 @@ from .permissions import IsSuperUserOrReadOnly
 
 class AdvertiseViewSet(ModelViewSet):
     queryset = Advertise.objects.all()
+    serializer_class = AdvertiseSerializer
+    permission_classes = [IsSuperUserOrReadOnly, ]
+
+
+class AdvertiseSliderApiView(generics.ListAPIView):
+    queryset = Advertise.objects.filter(is_slider=True)
     serializer_class = AdvertiseSerializer
     permission_classes = [IsSuperUserOrReadOnly, ]
