e5c9faa87fba123d9bdf2d683032b3cd2c8c2fc1
mrsins <amin.d.d.y1357@gmail.com>
2023-11-15 21:07:17 +0330

    fixed bugs

--- src/coupon/models.py
+++ src/coupon/models.py
@@ -55,8 +55,8 @@ class Coupon(models.Model):
     category = models.ManyToManyField(to=Category, verbose_name="دسته بندی")
     description = models.CharField(max_length=1000, blank=True, null=True, verbose_name="توضیحات")
     terms_of_use = models.TextField(blank=True, null=True, verbose_name="شرایط استفاده")
-    coupon_rate = models.DecimalField(blank=True, null=True, max_digits=2, decimal_places=1, verbose_name="امتیاز کوپن")
-    rate_count = models.PositiveIntegerField(null=True, blank=True, verbose_name="تعداد رای دهندگان")
+    coupon_rate = models.DecimalField(default=0, null=True, max_digits=2, decimal_places=1, verbose_name="امتیاز کوپن")
+    rate_count = models.PositiveIntegerField(default=0, blank=True, verbose_name="تعداد رای دهندگان")
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.slug = slugify(self.title, allow_unicode=True)
@@ -155,7 +155,7 @@ class Comment(models.Model):
     verified = models.BooleanField(default=False, verbose_name="تایید شده / نشده")
 
     def __str__(self):
-        return f"{self.coupon}({self.user})-{'sub comment' if self.parent else 'main comment'}"
+        return str(self.id)
 
     class Meta:
         verbose_name = "نظر"
