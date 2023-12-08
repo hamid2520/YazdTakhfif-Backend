3667802c9a6ac8ae265271c9c50bac4ce5a69d3d
mrsins <amin.d.d.y1357@gmail.com>
2023-12-08 18:17:29 +0330

    some bugs fixed

--- src/coupon/serializers.py
+++ src/coupon/serializers.py
@@ -5,10 +5,14 @@ from rest_framework.exceptions import ValidationError
 from src.business.serializers import BusinessSerializer
 from .models import Category, Coupon, LineCoupon, Rate, Comment, CouponImage
 from .exceptions import MaximumNumberOfDeletableObjectsError
+from ..basket.models import BasketDetail
+from ..business.models import Business
 from ..users.models import User
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    parent = serializers.SlugRelatedField(slug_field="slug", queryset=Category.objects.all())
+
     class Meta:
         model = Category
         fields = ["title", "slug", "parent"]
@@ -16,6 +20,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class CustomerCategorySerializer(serializers.ModelSerializer):
+    parent = serializers.SlugRelatedField(slug_field="slug", queryset=Category.objects.all())
     sub_categories = serializers.SerializerMethodField()
 
     class Meta:
@@ -30,6 +35,8 @@ class CustomerCategorySerializer(serializers.ModelSerializer):
 
 
 class CouponImageSerializer(serializers.ModelSerializer):
+    coupon = serializers.SlugRelatedField(slug_field="slug", queryset=Coupon.objects.all())
+
     class Meta:
         model = CouponImage
         fields = "__all__"
@@ -74,8 +81,8 @@ class CouponSerializer(serializers.ModelSerializer):
         return list(rates_list.values())
 
     def get_images(self, obj: Coupon):
-        images = obj.couponimage_set.all().values("image")
-        images_list = [url["image"] for url in images]
+        images = obj.couponimage_set.all()
+        images_list = [item.image.url for item in images]
         # serializer = CouponImageSerializer(instance=images, many=True)
         return images_list
 
@@ -94,12 +101,17 @@ class CouponSerializer(serializers.ModelSerializer):
 
 
 class CouponCreateSerializer(serializers.ModelSerializer):
+    business = serializers.SlugRelatedField(slug_field="slug", queryset=Business.objects.all())
+    category = serializers.SlugRelatedField(slug_field="slug", queryset=Category.objects.all(), many=True)
+
     class Meta:
         model = Coupon
         fields = ["title", "business", "expire_date", "category", "description", "terms_of_use"]
 
 
 class LineCouponSerializer(serializers.ModelSerializer):
+    coupon = serializers.SlugRelatedField(slug_field="slug", queryset=Coupon.objects.all())
+
     def save(self, **kwargs):
         try:
             return super().save(**kwargs)
@@ -113,7 +125,36 @@ class LineCouponSerializer(serializers.ModelSerializer):
         read_only_fields = ["slug", "price_with_offer", "sell_count"]
 
 
+class LineCouponShowSerializer(serializers.ModelSerializer):
+    coupon = serializers.SlugRelatedField(slug_field="slug", read_only=True)
+    basket_detail_count = serializers.SerializerMethodField(read_only=True)
+    in_stock = serializers.SerializerMethodField(read_only=True)
+    max = serializers.SerializerMethodField(read_only=True)
+
+    def get_in_stock(self, obj: LineCoupon):
+        available_count = obj.count - obj.sell_count
+        return True if available_count > 0 else False
+
+    def get_basket_detail_count(self, obj: LineCoupon):
+        basket_detail = BasketDetail.objects.filter(line_coupon_id=obj.id)
+        if basket_detail.exists():
+            return basket_detail.first().count
+        return 0
+
+    def get_max(self, obj: LineCoupon):
+        available_count = obj.count - obj.sell_count
+        return available_count
+
+    class Meta:
+        model = LineCoupon
+        fields = ["slug", "title", "coupon", "is_main", "count", "price", "offer_percent", "price_with_offer",
+                  "sell_count", "basket_detail_count", "in_stock", "max"]
+        read_only_fields = ["slug", "price_with_offer", "sell_count"]
+
+
 class RateSerializer(serializers.ModelSerializer):
+    coupon = serializers.SlugRelatedField(slug_field="slug", queryset=Coupon.objects.all())
+
     class Meta:
         model = Rate
         fields = "__all__"
@@ -122,6 +163,7 @@ class RateSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    coupon = serializers.SlugRelatedField(slug_field="slug", queryset=Coupon.objects.all())
 
     def validate_user(self, value):
         request = self.context["request"]
