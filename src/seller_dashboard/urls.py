35ba8ea494210b670dab6838d3bac0e989c18044
Alireza <areza4693@gmail.com>
2023-12-19 21:10:01 +0330

    new api in seller dashbord for user comment

---
+++ src/seller_dashboard/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserCommentList
+
+urlpatterns = [
+    path('user-comment-list', UserCommentList.as_view(), name='user_comment_list')
+]
--- src/seller_dashboard/views.py
+++ src/seller_dashboard/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from src.coupon.models import Comment
+from .serializers import CommentSerializer
+from rest_framework import status
+
 
 # Create your views here.
+
+class UserCommentList(APIView):
+    def get(self, request: Request):
+        comment = Comment.objects.filter(verified=True, coupon__business__admin=self.request.user)
+        comment = CommentSerializer(instance=comment, many=True)
+        return Response(data=comment.data, status=status.HTTP_200_OK)
