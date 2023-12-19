51d6a40d57cc5ceebccda719323711e48a5e194f
Alireza <areza4693@gmail.com>
2023-12-20 00:20:49 +0330

    fixed

--- src/seller_dashboard/serializers.py
+++ src/seller_dashboard/serializers.py
@@ -5,10 +5,10 @@ from src.coupon.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     user_full_name = serializers.SerializerMethodField()
+    coupon = serializers.SlugRelatedField("slug", read_only=True)
 
     class Meta:
         model = Comment
         fields = "__all__"
-
     def get_user_full_name(self, obj):
         return f'{obj.user.first_name} {obj.user.last_name}'
--- src/seller_dashboard/urls.py
+++ src/seller_dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserCommentList
 
 urlpatterns = [
-    path('user-comment-list', UserCommentList.as_view(), name='user_comment_list')
+    path('user-comment-list/', UserCommentList.as_view(), name='user_comment_list')
 ]
