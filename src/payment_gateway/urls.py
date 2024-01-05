96134c4067f34b85d29ee9210ff70512fda70d20
mrsins <amin.d.d.y1357@gmail.com>
2024-01-06 00:52:10 +0330

    payment gateway fixed

--- src/payment_gateway/urls.py
+++ src/payment_gateway/urls.py
@@ -5,7 +5,7 @@ from .views import go_to_gateway_view_v2, callback_gateway_view_v2, payment_resu
 
 urlpatterns = [
     path('', az_bank_gateways_urls()),
-    path('go-to-gateway/<slug:basket_slug>/', go_to_gateway_view_v2, name="go-to-gateway-v2"),
+    path('go-to-gateway/<slug:slug>/', go_to_gateway_view_v2, name="go-to-gateway-v2"),
     path('callback-gateway/<str:token>/', callback_gateway_view_v2, name="callback-gateway-v2"),
     path('payment-result/<str:token>/<int:status>/', payment_result, name="payment_result"),
     path('final-count-validation/<slug:basket_slug>/', BasketCountValidationAPIView.as_view(),
