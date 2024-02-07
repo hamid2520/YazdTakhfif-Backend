84a206854045872d146ce662b98644805c6dcc3e
mrsins02 <amin.d.d.y1357@gmail.com>
2024-02-07 18:58:24 +0330

    bugs fixed

--- src/business/admin.py
+++ src/business/admin.py
@@ -13,15 +13,23 @@ class BusinessAdmin(admin.ModelAdmin):
     readonly_fields = ["slug", "deposit", "withdraw", "balance"]
 
     def deposit(self, obj):
-        deposit = Transaction.objects.filter(user_id=obj.admin_id, type=1, status=2).aggregate(Sum("amount"))[
-            "amount__sum"]
+        if obj.admin.is_superuser:
+            deposit = Transaction.objects.filter(type=1, status=2).aggregate(Sum("amount"))[
+                "amount__sum"]
+        else:
+            deposit = Transaction.objects.filter(user_id=obj.admin_id, type=1, status=2).aggregate(Sum("amount"))[
+                "amount__sum"]
         return deposit if deposit else 0
 
     deposit.short_description = "واریز"
 
     def withdraw(self, obj):
-        withdraw = Transaction.objects.filter(user_id=obj.admin_id, type=2, status=2).aggregate(Sum("amount"))[
-            "amount__sum"]
+        if obj.admin.is_superuser:
+            withdraw = Transaction.objects.filter(type=2, status=2).aggregate(Sum("amount"))[
+                "amount__sum"]
+        else:
+            withdraw = Transaction.objects.filter(user_id=obj.admin_id, type=2, status=2).aggregate(Sum("amount"))[
+                "amount__sum"]
         return withdraw if withdraw else 0
 
     withdraw.short_description = "برداشت"
