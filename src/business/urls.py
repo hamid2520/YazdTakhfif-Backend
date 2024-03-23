7154d80a982d26f9d7e92ce9117de51de2a0ed5b
rezapb <rezapoorbafrani021@gmail.com>
2024-03-23 10:17:26 +0330

    fixed

--- src/business/urls.py
+++ src/business/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import SimpleRouter
 from .views import BusinessViewSet, DepositViewSet, CorporateViewSet
 
 router = SimpleRouter()
-router.register(prefix="", viewset=BusinessViewSet, basename="business")
 router.register(r'deposit', DepositViewSet, 'deposit_api')
+router.register(prefix="", viewset=BusinessViewSet, basename="business")
 
 urlpatterns = [
     path('corporate/', CorporateViewSet.as_view())
