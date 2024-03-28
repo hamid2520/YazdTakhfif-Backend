c2d0f51232b0bc1e1ec6cbd597e6e4629850d6e0
rezapb <rezapoorbafrani021@gmail.com>
2024-03-28 21:10:08 +0330

    change urls

--- src/urls.py
+++ src/urls.py
@@ -26,38 +26,31 @@ router.registry.extend(files_router.registry)
 
 urlpatterns = [
                   # site urls
-                  path('business/', include('src.business.urls')),
-                  path('coupon/', include('src.coupon.urls')),
-                  path('basket/', include('src.basket.urls')),
-                  path('payment/', include('src.payment.urls')),
-                  path('payment-gateway/', include('src.payment_gateway.urls')),
-                  path('offer/', include('src.offer.urls')),
-                  path('advertise/', include('src.advertise.urls')),
-                  path('search/', include('src.search_engine.urls')),
-                  path('seller-dashboard/', include('src.seller_dashboard.urls')),
-                  path('wallet/', include('src.wallet.urls')),
+                  path('api/business/', include('src.business.urls')),
+                  path('api/coupon/', include('src.coupon.urls')),
+                  path('api/basket/', include('src.basket.urls')),
+                  path('api/payment/', include('src.payment.urls')),
+                  path('api/payment-gateway/', include('src.payment_gateway.urls')),
+                  path('api/offer/', include('src.offer.urls')),
+                  path('api/advertise/', include('src.advertise.urls')),
+                  path('api/search/', include('src.search_engine.urls')),
+                  path('api/seller-dashboard/', include('src.seller_dashboard.urls')),
+                  path('api/wallet/', include('src.wallet.urls')),
                   # admin panel
                   path('admin/', admin.site.urls),
                   # url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
                   # summernote editor
-                  path('summernote/', include('django_summernote.urls')),
+                  path('api/summernote/', include('django_summernote.urls')),
                   # api
                   path('api/v1/', include('src.users.urls')),
                   url(r'^api/v1/password_reset/',
                       include('django_rest_passwordreset.urls', namespace='password_reset')),
                   # auth
-                  path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                  path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
                   path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
                   path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   # social login
-                  url('', include('social_django.urls', namespace='social')),
-                  # swagger docs
-                  url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
-                      name='schema-json'),
-                  url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-                  url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-                  url(r'^health/', include('health_check.urls')),
+                  url('api/', include('social_django.urls', namespace='social')),
                   # the 'api-root' from django rest-frameworks default router
-                  re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
