808136f9e4f54d64be320e057a53f7428ab9728c
rezapourbafrani021 <rezapourbafrani021@gmail.com>
2025-02-15 16:16:07 +0330

    fixes

--- src/wallet/serializers.py
+++ src/wallet/serializers.py
@@ -147,7 +147,7 @@ class WalletSerializer(serializers.ModelSerializer):
 
     def get_balance(self, obj: Business):
         requested_deposits = DepositRequest.objects.filter(sender__id=self.context['user_id'], status=1) \
-            .aggregate(amount=Sum('requested_price'))['amount']
+            .aggregate(amount=Sum('requested_price', default=0))['amount']
         return self.get_total_sell(obj, with_commission=True) - self.get_total_withdraw(obj) - requested_deposits
 
     class Meta:
